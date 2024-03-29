1178af4cca748e647c2ce1df853046ae81f2d102
nstranges <nicholastranges4@hotmail.com>
2024-03-29 18:21:32 -0400

    Created my part. Need timer and comms.

--- FinalProjectCode.c
+++ FinalProjectCode.c
@@ -1,82 +1,122 @@
 #define ADC_BASE	0xFF204000
-#define JP1_BASE    0xFF200060
 #define SW_BASE     0xFF200040
 
-void initLedGpio();
-void writeLeds(int ledsToWrite);
-int readADC();
-int readSlider();
-int detrLedPos(int adcVal);
+const int switchNum = 8;
+
+// Init functions
+void initADCAutoWrite();
+void readADC(int* switchesFlipped, int* adcVals);
+void readSlider(int* switchesFlipped, int* avgCount);
+void calcAvgs(int* adcVals, int* avgCount, int* avgs);
+void sendData(int* avgs, int* switchesFlipped);
 
 int main() {
-	// init gpio
-	initLedGpio();
+	// init adc
+	initADCAutoWrite();
 
-	// Read the adc and write to the leds
+	// To store all adc values and avg counters. Init to 0
+	int[switchNum] adcVals = {0};
+	int[switchNum] avgCount = {0};
+	int[switchNum] switchesFlipped = {0};
+	int[switchNum] avgs = {0};
+	int timerDone = 0;
+
+	// Main loop
 	while (1) {
-		int adcVal = readADC();
-		int ledPattern = detrLedPos(adcVal);
-		writeLeds(ledPattern);		
+		readSliders(switchesFlipped, avgCount);
+		readADC(switchesFlipped, adcVals);
+
+		// Check if timer is done, send data
+		if (timerDone == 1) {
+			// Calculates averages
+			calcAvgs(adcVals, avgCount, avgs);
+		}
 	}
 }
 
-// Puts GPIO into output mode
-void initLedGpio() {
-	volatile int *ledCtlPtr = (int*) (JP1_BASE + 0x04);
+void initADCAutoWrite() {
+	volatile int *adcCtlPtr = (int*) (ADC_BASE + 0x04);
 
-	*ledCtlPtr = 0b1111111111; // setting the leds to output
+	*adcCtlPtr = 0b1; // setting the adc to auto update
 }
 
-// Writes all leds
-void writeLeds(int ledsToWrite) {
-	volatile int *ledPtr = (int*) JP1_BASE;
-
-	*ledPtr = ledsToWrite; // setting the leds
-}
-
-// Returns the ADC value
-int readADC() {
+// Returns the ADC values
+void readADC(int* switchesFlipped, int* adcVals) {
 	int offset;
 
-	// Reading the slider
-	if (readSlider() == 0) {
-		offset = 0x00;
-	}
-	else {
-		offset = 0x04;
+	// Checking each switch register
+	for (i = 0; i < switchNum; i++) {
+		// Reading the sliders
+		if (switchesFlipped[i] == 1) {
+			// Giving the correct offset for this register
+			offset = 0x04 * i;
+
+			// To read the adc
+			volatile int *adcPtr = (int*) (ADC_BASE + offset);
+			int adc;
+			int adcVal;
+			int doneBit;
+
+			// loop until the done bit goes high
+			do {
+				adc = *adcPtr;
+				adcVal = (adc & 0b111111111111);
+				doneBit = (adc & (1 << 15)); // Shift by 16 for the simulator
+			} while (doneBit == 0);
+
+			adcVals[i] += adcVal; 
+		}	
 	}
 
-	volatile int *adcPtr = (int*) (ADC_BASE + offset);
-	int adc;
-	int adcVal;
-	int doneBit;
-
-	// loop until the done bit goes high
-	do {
-		adc = *adcPtr;
-		adcVal = (adc & 0b111111111111);
-		doneBit = (adc & (1 << 15)); // Shift by 16 for the simulator
-	} while (doneBit == 0);
-	
-	return adcVal;
+	return adcVals;
 }
 
-// Reads the slider value
-int readSlider() {
+// Reads all the sliders
+void readSliders(int* switchesFlipped, int* avgCount) {
 	volatile int *sliderPtr = (int*) SW_BASE;
 
-	int slider = *sliderPtr;
+	int sliders = *sliderPtr;
 
-	// Reading the first bit
-	return (slider & 1);
+	// Assign all the sliders
+	for (int i = 0; i < switchNum; i++) {
+		switchesFlipped[i] = sliders & (0b1 << i);
+
+		// Increment count if switch is on
+		if (switchesFlipped[i] == 1) {
+			avgCount[i]++;
+		}
+	}
 }
 
-// Determines what leds to light up
-int detrLedPos(int adcVal) {
-	// Determine leds off
-	int percentLedsOn = (adcVal*100) / 4095; // what percentage
-	int numLedsOn = percentLedsOn / 10;
-	int shiftAmount = 10 - numLedsOn;
+// Calculates the averages and clears other arrays
+void calcAvgs(int* adcVals, int* avgCount, int* avgs) {
+	for (int i = 0; i < switchNum; i++) {
+		// Checking for divide by 0
+		if (avgCount == 0) {
+			avgs[i] = 0;
+		}
+		else {
+			avgs[i] = adcVals / avgCount;
+		}
 
-	return (0b1111111111 >> shiftAmount);
+		// Clearing averages
+		adcVals[i] = 0;
+		avgCount[i] = 0;
+	}
+}
+
+// Packaging and sending the data over RS-232
+void sendData(int* avgs, int* switchesFlipped) {
+	for (int i = 0; i < switchNum; i++) {
+		// Only send if selected
+		if (switchesFlipped[i] == 1) {
+			// this byte is number, 5 bits starting with the msb of the val
+			char byte1 = (i << 5) + ((avgs[i] & 0b111110000000) >> 7);
+
+			// this byte is the rest of the number ending with the lsb, 1 extra space 
+			char byte2 = (avgs[i] & 0b1111111) << 1;
+
+			// something like sendData(byte1, byte2);
+		}
+	}
 }
