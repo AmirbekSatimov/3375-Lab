92c713364986f772e2fe9b49fceb7436813bf8fe
nstranges <nicholastranges4@hotmail.com>
2024-04-07 17:30:56 -0400

    Added the testing functionality

--- FinalProjectCode.c
+++ FinalProjectCode.c
@@ -1,10 +1,14 @@
 #define ADC_BASE	0xFF204000
 #define SW_BASE     0xFF200040
 #define MPCORE_PRIV_TIMER     0xFFFEC600
-#define JTEG_UART 	0xFF201007
+#define JTEG_UART 	0xFF201000
+#define HEX3_HEX0_BASE        0xFF200020
 
 const int switchNum = 8;
 const int secLen = 200000000;
+const char hex_code[16] = {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F, 0x77, 0x7C, 0x39, 0x5E, 0x79, 0x71};
+
+
 
 // Define structs
 typedef struct a9Timer{
@@ -22,6 +26,7 @@ void readSliders(int* switchesFlipped, int* avgCount);
 void calcAvgs(int* adcVals, int* avgCount, int* avgs);
 void sendData(int* avgs, int* switchesFlipped);
 void sendOverSerial(char byte1, char byte2);
+void setHex(int binVal, int inputNum);
 
 int main() {
 	// init adc
@@ -37,9 +42,6 @@ int main() {
 	// Starting the clock
 	timer->control = 3;
 
-	// Flag to see if one second has passed
-	status = timer->status;
-
 	// To store all adc values and avg counters. Init to 0
 	int adcVals[switchNum];
 	int avgCount[switchNum];
@@ -52,10 +54,12 @@ int main() {
 		readSliders(switchesFlipped, avgCount);
 		readADC(switchesFlipped, adcVals);
 
+		// Flag to see if one second has passed
+		status = timer->status;
+		
 		// Check if timer is done, send data
 		if (status == 1) {
-			
-			// Resetting status to 0
+			// Resetting status to 1
 			timer->status = 1;
 			
 			// Calculates averages
@@ -111,7 +115,8 @@ void readSliders(int* switchesFlipped, int* avgCount) {
 
 	// Assign all the sliders
 	for (int i = 0; i < switchNum; i++) {
-		switchesFlipped[i] = sliders & (0b1 << i);
+		// Making sure to shift back to read
+		switchesFlipped[i] = (sliders & (0b1 << i)) >> i;
 
 		// Increment count if switch is on
 		if (switchesFlipped[i] == 1) {
@@ -150,6 +155,10 @@ void sendData(int* avgs, int* switchesFlipped) {
 
 			// sending the data over serial
 			sendOverSerial(byte1, byte2);
+			
+			// Temp putting on hex
+			int hexVal = formatHex(avgs[i]);
+			setHex(hexVal, i);
 		}
 	}
 }
@@ -168,7 +177,7 @@ void sendOverSerial(char byte1, char byte2) {
 
 	if (write_space != 0xFFFF000) {
 		ptr--;
-		*ptr = data;
+		*ptr = data;	
 	}
 }
 
@@ -180,4 +189,27 @@ void zeroArrays(int* adcVals, int* avgCount, int* switchesFlipped, int* avgs){
 		switchesFlipped[i] = 0;
 		avgs[i] = 0;
 	}
+}
+
+// Sets a hex value
+void setHex(int binVal, int inputNum) {
+	volatile int *hex_ptr = (int*) (HEX3_HEX0_BASE); // setting the HEX address
+	
+	// If they want to clear the hex
+	if (binVal == -1) {
+		*hex_ptr = 0;
+	}
+	else {
+		// Setting the hex with the device num and voltage val
+		*hex_ptr = (hex_code[binVal] + (hex_code[inputNum] << 8));
+	}
+}
+
+// Formatting and sending to hex
+int formatHex(int avg) {
+	// Calculating the voltage for display purposes
+	int ans = (avg*100) / 4095;
+	ans /= 10;
+	
+	return ans;
 }
